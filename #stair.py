4be6a538e21d7403ec189cd205073a6515b475c1
mroanvui <ravi9955408342@gmail.com>
2023-09-04 20:17:28 +0530

    3

--- #stair.py
+++ #stair.py
@@ -3,3 +3,5 @@ for i in range(n): # i = 0 1 2 3
     for j in range(i+1): # i = 0+1 1+1 2+1 3+1
         print("#",end='')
     print("")
+
+#for making the loop two dimensional we add another loop inside a loop
